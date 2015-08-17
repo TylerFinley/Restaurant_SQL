2e0479b5cf130a935daf5673070a89503cae495e
Tyler Finley & Vaughn Dice <student@epicodus.com>
2015-08-17 13:47:11 -0700

    implement save all and == method for food class

--- lib/food.rb
+++ lib/food.rb
@@ -6,4 +6,25 @@ class Food
     @cost = attributes.fetch(:cost)
     @rating = attributes.fetch(:rating)
   end
+  define_singleton_method(:all) do
+    returned_foods = DB.exec("SELECT * FROM foods;")
+    foods = []
+    returned_foods.each() do |food|
+      type = food.fetch('type')
+      cost = food.fetch('cost').to_f
+      rating = food.fetch('rating').to_i
+      foods.push(Food.new({:type => type, :cost => cost, :rating => rating}))
+    end
+    foods
+  end
+  define_method(:save) do
+    DB.exec("INSERT INTO foods(type, cost, rating) VALUES ('#{type}', #{cost}, #{rating});")
+  end
+  define_method(:==) do |other|
+    same_class = self.class().eql?(other.class())
+    same_type = self.type().eql?(other.type())
+    same_cost = self.cost().eql?(other.cost())
+    same_rating = self.rating().eql?(other.rating())
+    same_class.&(same_type).&(same_cost).&(same_rating)
+  end
 end
--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -21,4 +21,26 @@ describe(Food) do
       expect(test_food.rating()).to(eq(5))
     end
   end
+
+  describe(".all") do
+    it('returns all food instances') do
+      expect(Food.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('adds a food to the array of saved restaurants') do
+      food = Food.new({:type => 'broccoli', :cost => 1.03, :rating => 5})
+      food.save()
+      expect(Food.all()).to(eq([food]))
+    end
+  end
+
+  describe('#==') do
+    it('returns true if same name, location and phone') do
+      food1 = Food.new({:type => 'broccoli', :cost => 1.03, :rating => 5})
+      food2 = Food.new({:type => 'broccoli', :cost => 1.03, :rating => 5})
+      expect(food1).to(eq(food2))
+    end
+  end
 end
